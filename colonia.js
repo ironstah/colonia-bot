f9dfa8f3dc392068f2cb4f1b3b40d6f2c4a9c9d6
Imperial Roblox Github <41947097+ImperialVanquisher@users.noreply.github.com>
2019-06-06 19:20:05 -0700

    Update colonia.js

--- colonia.js
+++ colonia.js
@@ -136,7 +136,7 @@ function checkReputation(id, rep) {
 }
 
 async function startApp () {
-    await rbx.cookieLogin(password);
+    await rbx.cookieLogin("_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_"+password);
     let currentUser = await rbx.getCurrentUser()
 }
 startApp()
